85cb5347fc1f8a9bd33df359d0d0267faac6ace4
Edward Chan <chankaward@gmail.com>
2013-12-06 14:30:45 +0800

    finish homework of chapter six

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  #before_save is a CallBack method
+  before_save { email.downcase! }
 
-  VALID_EMAIL_REGEX = /\A[\w+-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
 
+  # Very powerful method
   has_secure_password
 end
