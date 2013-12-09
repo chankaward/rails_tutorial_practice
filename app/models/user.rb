58673a28f34d0d56a9bbf2be0ee30fca99f3de72
Edward Chan <chankaward@gmail.com>
2013-12-09 18:06:35 +0800

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  #before_save is a CallBack method
+  #CallBack method
   before_save { email.downcase! }
+  before_create :create_remember_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50 }
@@ -9,4 +10,18 @@ class User < ActiveRecord::Base
 
   # Very powerful method
   has_secure_password
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
