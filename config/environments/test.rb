e85cc7be91e0d45f586ea7cf9802cd1fe63a401f
Edward Chan <chankaward@gmail.com>
2013-12-09 10:39:55 +0800

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  # Speed up tests by lowering bcrypt's cost funcion.
+  ActiveModel::SecurePassword.min_cost = true
 end
